c116614d33d7a484960240392ca94a8501e8a500
Anni1305 <113096755+Anni1305@users.noreply.github.com>
2022-09-08 14:25:29 +0530

    Add files via upload

---
+++ demo.js
@@ -0,0 +1,14 @@
+console.log("welcome to the world of programming ");
+
+var a=5;
+var b=6;
+var c=a+b;
+console.log(`${a} and ${b} addition is ${c}`);
+console.log(`${a} and ${b} addition is ${c} sum is ${a+b}`);
+var msg="value of c is :"+c;
+if (c==11) {
+    msg+="sum is 11";
+    
+} else {
+    msg+="sum is not matched";
+}
